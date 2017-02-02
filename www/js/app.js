2f8980b82604718713a74b852f251972c385146f
icable24 <icable130@gmail.com>
2017-02-03 02:15:21 +0800

    Vid 24

--- www/js/app.js
+++ www/js/app.js
@@ -23,6 +23,25 @@ angular.module('starter', ['ionic'])
   });
 })
 
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('tabs', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+    .state('tabs.list', {
+      url: '/list',
+      views: {
+        'list-tab' : {
+          templateUrl: 'templates/list.html',
+          controller: 'ListController'
+        }
+      }
+    })
+  $urlRouterProvider.otherwise('/tab/list');
+})
+
 .controller('ListController', ['$scope', '$http', 
   function($scope, $http){
     $http.get('js/data.json').success(function(data){
@@ -32,9 +51,16 @@ angular.module('starter', ['ionic'])
         $scope.artists.splice($scope.artists.indexOf(item), 1);
       };
 
+      $scope.doRefresh = function(){
+        $http.get('js/data.json').success(function(data){
+         $scope.artists = data.artists;
+         $scope.$broadcast('scroll.refreshComplete');
+        });
+      };
+
       $scope.toggleStar = function(item){
         item.star = !item.star;
-      }
+      };
 
       $scope.moveItem = function(item, fromIndex, toIndex){
         $scope.artists.splice(fromIndex, 1);
